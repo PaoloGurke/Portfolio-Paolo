cf7f3fa70f04b5e3f79686406554e67df07775cf
AnimeReality <narutodragonblaze.fp@gmail.com>
2025-04-07 16:11:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,16 +44,7 @@ function App() {
 
 
 
-  const handlePlayPause = () => {
-    const audio = audioRef.current;
-    if (audio.paused) {
-      audio.play();
-      setIsPlaying(true);
-    } else {
-      audio.pause();
-      setIsPlaying(false);
-    }
-  };
+  
 
   const handleResumeClick = () => {
     setShowTransition(true);
@@ -69,23 +60,12 @@ function App() {
       {/* Navbar */}
       <Navbar />
 
-      {/* Background music toggle button */}
-      <button onClick={handlePlayPause} style={{ padding: '10px', fontSize: '16px' }}>
-        {isPlaying ? 'Pause Music' : 'Play Music'}
-      </button>
-
-      {/* Background music */}
-      <audio ref={audioRef} id="bgAudio" loop autoPlay>
-        <source src="/bgmperf.mp3" type="audio/mp3" />
-        Your browser does not support the audio element.
-      </audio>
+    
 
       {/* Resume Section */}
       <section className="slide" id="resume">
-        <h2>My Resume</h2>
-        <button id="resumeButton" onClick={handleResumeClick}>
-          View Resume
-        </button>
+                <button id="resumeButton" onClick={handleResumeClick}>
+                  </button>
       </section>
 
       {/* Video transition overlay */}
